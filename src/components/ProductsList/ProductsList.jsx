5b9d7a3eb0b1dbd5cea4cf4b8426ddda152b2839
Marc Carranza <marccarranzaferrez@gmail.com>
2023-05-17 16:17:21 +0200

    feat: styling tweaks, working on functionality

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -11,6 +11,22 @@ const QUANTITY_LIST = [1, 2, 3, 4, 5];
 
 export default function ProductsList({ data, isLoading = false }) {
   const [productToAdd, setProductToAdd] = useState(null);
+  // TODO: productsQty would be too much if the list is too large?
+  const [productsQty, setProductsQty] = useState({});
+
+  // Functionalities
+  const onChangeQuantity = (productId, qty) => {
+    const updatedProductsQty = {
+      ...productsQty,
+      [productId]: qty,
+    };
+
+    setProductsQty(updatedProductsQty);
+  };
+
+  const onAddProduct = (id) => {
+    // TODO: If ID is in, if not qty is the default value ()
+  };
 
   return (
     <div className={styles.list_wrapper}>
@@ -24,17 +40,26 @@ export default function ProductsList({ data, isLoading = false }) {
                   <p>{description}</p>
                 </div>
                 <div className={styles.list__qty}>
-                  <select className={styles.qty__select} id="qty">
+                  <select
+                    className={styles.qty__select}
+                    id="qty"
+                    onChange={(e) =>
+                      onChangeQuantity(id, e.currentTarget.value)
+                    }
+                  >
                     {QUANTITY_LIST.map((qty) => (
                       <option key={`${id}-${qty}`} value={qty}>
                         {qty}
                       </option>
                     ))}
                   </select>
-                  <button className={styles.qty__addBtn}>
+                  <button
+                    className={styles.qty__addBtn}
+                    onClick={() => onAddProduct(id)}
+                  >
                     <Image
                       src="/plus.svg"
-                      altText="Add icon"
+                      alt="Add icon"
                       width={20}
                       height={20}
                     />
